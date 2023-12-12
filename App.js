44ffbc32f4e8f4c12f809bd66a6861f805ef1e8f
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-12 21:01:49 +0530

    Add Packages and Readme

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function App() {
+const YourApp = () => {
+  // State to store the background color
+  const [backgroundColor, setBackgroundColor] = useState("#000000");
+
+  // Function to change the background color
+  const changeColor = () => {
+    // Generate a random color
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setBackgroundColor(randomColor);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor }]}>
+      <TouchableOpacity style={styles.button} onPress={changeColor}>
+        <Text style={styles.buttonText}>Press This</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+
+  button: {
+    alignSelf: "center",
+    width: 200,
+    padding: 10,
+    margin: 20,
+    borderRadius: 50,
+    backgroundColor: "white",
+    borderColor: "white",
+    borderWidth: 1,
+    // Shadow
+    shadowColor: "black", // Shadow color
+    shadowOffset: { width: 0, height: 2 }, // Shadow offset
+    shadowOpacity: 0.2, // Shadow opacity
+    shadowRadius: 4, // Shadow radius
+    elevation: 5, // Android elevation
+  },
+
+  buttonText: {
+    color: "black",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "500",
   },
 });
+
+export default YourApp;
---
+++ README.md
@@ -0,0 +1,10 @@
+## Random Color Changer
+
+This is a small program that changes to a random color when the button is pressed.
+
+### Technologies Used
+
+- React Native
+- JavaScript
+
+Feel free to explore the code and contribute!
